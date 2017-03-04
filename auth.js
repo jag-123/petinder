8182f50d964532cb4827d3ef18981da36e8ae80e
arianaolson419 <ariana.olson@students.olin.edu>
2017-03-04 14:43:42 -0500

    auth

---
+++ auth.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'FACEBOOK_APP_ID' : '589753241229559',
+    'FACEBOOK_APP_SECRET' : '4dabef12aaf42f71bf4d9ff7790d9ec8',
+    'FACEBOOK_CALLBACK_URL' : 'http://localhost:3000/auth/facebook/callback'
+  }
