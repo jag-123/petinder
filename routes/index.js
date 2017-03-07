c9b74bdd0caf04db122db8505e44d1be9089fb62
arianaolson419 <ariana.olson@students.olin.edu>
2017-03-06 23:48:32 -0500

    got rid of all handlebars related rendering, changed some res.sends to res.json when we needed user info in React

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,13 @@ var LocalStrategy = require('passport-local').Strategy;
 
 var router = express.Router();
 
+var User = require("../models/userModel");
 // returns the homepage
 router.get('/', function(req, res, next) {
 	res.json({'text' : 'PeTinder'});
 });
 
-// returns the login page
+// returns the login page. This is not being used right now!!!
 router.get('/login', function(req, res) {
 	res.json({'text': 'placeholder'});
 });
@@ -20,12 +21,14 @@ router.get('/auth/facebook',
 	passport.authenticate('facebook')
 );
 
+// the redirects are weird right now... will probably make more 
+// sense once we figure out changing pages with React
 router.get('/auth/facebook/callback',
 	passport.authenticate('facebook', {successRedirect: '/',
-										failureRedirect: '/login'})
+										failureRedirect: '/'})
 );
 
-// check which user is logged in
+// GET information on the user who is logged in
 router.get('/user', 
 	function(req, res, next) {
 		if(req.isAuthenticated()) { 
@@ -33,26 +36,39 @@ router.get('/user',
 		}
 		res.send(401);
 	}, function(req, res) {
-		res.send(req.user);
+		res.json({
+			username:req.user.username, 
+			name: req.user.name,
+			id: req.user._id
+		});
 });
 
 
 // log in with local strategy
 router.post('/login', passport.authenticate('local'), 
 	function(req, res) {
-		res.redirect('/');
+		console.log(req);
+		res.json({
+			username: req.user.username,
+			name: req.user.name,
+			id: req.user._id
+		});
 	}
 );
 
 // register new user
 router.post('/register', function(req, res) {
-  User.register(new User({username: req.body.username}), req.body.password, function(err, account) {
+  User.register(new User({username: req.body.username, name:req.body.name}), req.body.password, function(err, account) {
     if (err) {
-      return res.render("login", {});
+      console.error(err);
     }
 
     passport.authenticate('login', function(req, res) {
-      res.redirect('/');
+      res.json({
+      	username: account.username,
+      	name: account.name,
+      	id: account._id
+      });
     });
   });	
 })
@@ -60,7 +76,7 @@ router.post('/register', function(req, res) {
 // logout anyone who is logged in
 router.get('/logout', function(req, res) {
 	req.logout()
-	res.redirect('/')
+	res.send("logged out") // this is for testing. should be taken out later!!
 })
 
 module.exports = router;
