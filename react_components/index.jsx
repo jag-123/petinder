9168aca726b379abec0a2195f00ae73d5ebbaad5
arianaolson419 <ariana.olson@students.olin.edu>
2017-03-09 10:05:01 -0500

    added a component for getting a pet, functions to create api request url for a random pet

--- react_components/index.jsx
+++ react_components/index.jsx
@@ -1,3 +1,8 @@
+var md5 = require('md5'); // for hashing api signatures
+var auth = require('../auth');
+var secret = auth.PETFINDER_APP_SECRET;
+var key = auth.PETFINDER_APP_KEY;
+
 // the wrapper for everything
 var PeTinder = React.createClass({
   getInitialState: function() {
@@ -6,6 +11,7 @@ var PeTinder = React.createClass({
       username: null,
       name: null,
       userId: null
+      // preferences needed
     };
   },
   // update state with user data if someone is logged in
@@ -39,6 +45,7 @@ var PeTinder = React.createClass({
         <LoginLocal/>
         <LoginFacebook/>
         <RegisterNewUser/>
+        <GetPet/>
       </div>
     );
   }
@@ -272,6 +279,51 @@ var Logout = React.createClass({
     )
   }
 });
+// starting api request stuff
+var GetPet = React.createClass({
+  getInitialState: function() {
+    return {}
+  },
+  makeSignature: function(argsString) {
+    var sig = md5(secret + argsString);
+    return "&sig=" + sig;
+  },
+  makeArgsString: function(argObj = {}) {
+    var argsString = 'key=' + key;
+
+    for (var k in argObj) {
+      argsString += '&' + k + '=' + String(argObj[k]);
+    }
+
+    return argsString;
+  },
+  makeURL: function(func, argObj) {
+    var base = 'api.petfinder.com/' + func + '?'
+    var argsString = this.makeArgsString(argObj);
+    var signature = this.makeSignature(argsString);
+
+    return base + argsString + signature;
+  },
+  randomPet: function(event) {
+    // having trouble with CORS so no GET request written yet
+    // making url to request random pet id #
+    var getRandom = 'pet.getRandom';
+    var argsGetRandom = {'location': '02492'};
+    console.log(this.makeURL(getRandom, argsGetRandom));
+
+    // making url to request pet with this id number
+    var getPet = 'pet.get';
+    var argsGetPet = {id: '37235293'} // obtained from request
+    console.log(this.makeURL(getPet, argsGetPet));
+  },
+  render: function() {
+    return (
+      <div>
+        <input type="button" value="get a pet" onClick={this.randomPet}/>
+      </div>
+    )
+  }
+})
 ReactDOM.render(
   <PeTinder />,
   document.getElementById('content')
