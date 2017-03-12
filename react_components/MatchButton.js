14254a9492bde77c2dfbbc6b5e0a871f244c9402
arianaolson419 <ariana.olson@students.olin.edu>
2017-03-12 13:59:58 -0400

    added save functionality to the clickme button

--- react_components/MatchButton.js
+++ react_components/MatchButton.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
 export default React.createClass({
+	onSuccess: function(data) {
+		console.log(data);
+	},
+	onError: function(err, status) {
+		console.error(status);
+	},
 	saveMatch: function(event) {
 		event.preventDefault();
+		alert(JSON.stringify(this.props.match.size));
+
+		var matchData = {
+			user: this.props.user, // user _id
+			petId: this.props.match.id, // petfinder id
+			name: this.props.match.name,
+			age: this.props.match.age,
+			sex: this.props.match.sex,
+			size: this.props.match.size
+		}
+
+		$.post('/match', matchData)
+			.done(this.onSuccess)
+			.error(this.onError);
 		// to do in the morning: 
 		// * make a route to create a new pet in database
 		// * make a route to add match to list of matches in 
