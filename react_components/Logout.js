44186de199987ddeb6156361bcf3292713f34bb3
arianaolson419 <ariana.olson@students.olin.edu>
2017-03-16 23:02:10 -0400

    added description and cleaned up component

--- react_components/Logout.js
+++ react_components/Logout.js
@@ -2,17 +2,12 @@ import React from 'react';
 import { browserHistory } from 'react-router';
 
 export default React.createClass({
-  getInitialState: function() {
-    return {}
-  },
-  // I don't know if any information has to be logged here,
-  // but it's ok for testing for now
   logout: function(event) {
     event.preventDefault;
 
     $.get('/logout')
       .done(function(data) {
-        console.log(data);
+        return null;
       })
       .error(function(xhr, status, err) {
         console.error('GET /home', status, err.toString());
