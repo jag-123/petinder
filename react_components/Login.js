412e86fd9238e3028998b3c66d5d226e21772736
arianaolson419 <ariana.olson@students.olin.edu>
2017-03-16 22:57:42 -0400

    added description at top of file

--- react_components/Login.js
+++ react_components/Login.js
@@ -1,3 +1,10 @@
+/*
+The component that is displayed when a user is not logged in. The two options for
+logging in are using a username and password (local strategy) or using facebook.
+If a new user wishes to log in with a username and password, they must first
+register using the register form. 
+*/
+
 import React from 'react'
 import LoginLocal from './LoginLocal'
 import LoginFacebook from './LoginFacebook'
