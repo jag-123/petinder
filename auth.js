ff18f8d95f113316f9982056f813ed4a24090858
Jeremy Garcia <jeremiah.garcia@students.olin.edu>
2017-03-20 20:01:17 -0400

    fixed auth file

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 module.exports = {
     'FACEBOOK_APP_ID' : '589753241229559',
     'FACEBOOK_APP_SECRET' : '4dabef12aaf42f71bf4d9ff7790d9ec8',
-    'FACEBOOK_CALLBACK_URL' : 'http://localhost:3000/auth/facebook/callback',
+    'FACEBOOK_CALLBACK_URL' : 'https://whatapig.herokuapp.com/auth/facebook/callback',
 	'PETFINDER_APP_KEY' : '939f12ed7d810ee51d01c80015f7147b',
 	'PETFINDER_APP_SECRET' : '9e61b6dad8367139791e77cc42c82c60',
 }
